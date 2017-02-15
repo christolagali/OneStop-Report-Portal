5b5fe7664704ce1e0773dc1f6a964cd8abc9913b
christo Lagali <christo.lagali@gmail.com>
2017-02-14 17:25:05 -0800

    Report Data Array Created
    Following changes have been made:
    1. Created parsedEntries[] Array from Google Spreadsheet JSON objects
    2. Added parent_Item value as per divid values.

--- scripts/app1.js
+++ scripts/app1.js
@@ -17,18 +17,70 @@
             };
         }
 
+       
+        $scope.parsedEntries = [];
         $http.get(url)
         .success(function(response) {
            // console.log(response);
             var entries = response['feed']['entry'];
             //console.log(entries);
-            $scope.parsedEntries = [];
+           
+            
             for ($scope.key in entries) {
                 var content = entries[$scope.key];
-                 //console.log(content.title.$t);
-                 //console.log(parse('title')(content));
-                $scope.parsedEntries.push(content.title.$t);
+                 $scope.reportDet={
+                    "name":"",
+                    "divid":"",
+                    "itemid":"",
+                    "url":"",
+                    "parent_Item":""
+                };
+                //console.log(content);
+                //console.log(parse('title')(content));
+            
+                $scope.reportDet.name = content.gsx$items;
+                $scope.reportDet.divid = content.gsx$divid;
+                $scope.reportDet.itemid = content.gsx$itemid;
+                $scope.reportDet.url = content.gsx$url;
+                
+                $scope.parsedEntries.push($scope.reportDet);
+                $scope.addParentData($scope.key);
+                //Accessing items from the list: DivID content.gsx$divid, ITEMID content.gsx$itemid,Name content.gsx$items,URL content.gsx$url
+                //console.log($scope.parsedEntries);
                 
             }
         });
+
+        $scope.addParentData = function(key){
+            
+            //angular.forEach($scope.parsedEntries,function(value,key){
+            
+            switch($scope.parsedEntries[key].divid.$t)
+            {
+                case "ui-id-1":
+                    $scope.parsedEntries[key].parent_Item = "Enrollment_Information";
+                break;
+                case "ui-id-2":
+                    $scope.parsedEntries[key].parent_Item = "Admissions_Information";
+                break;
+                case "ui-id-3":
+                    $scope.parsedEntries[key].parent_Item = "Provost_Information";
+                break;
+                case "ui-id-4":
+                    $scope.parsedEntries[key].parent_Item = "Other_Student_Information";
+                break;
+                case "ui-id-5":
+                    $scope.parsedEntries[key].parent_Item = "Finance_Information";
+                break;
+                case "ui-id-6":
+                    $scope.parsedEntries[key].parent_Item = "Hr_Information";
+                break;
+                case "ui-id-7":
+                    $scope.parsedEntries[key].parent_Item = "Other_Useful_Information";
+                break;
+            }    
+            
+            //}); 
+            console.log($scope.parsedEntries);
+        };
     }]);
